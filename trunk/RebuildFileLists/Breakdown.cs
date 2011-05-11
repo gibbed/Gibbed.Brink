e1c29da31984390c54c06052c71e27b3532ad354
rick <rick@c6a1b0c9-557b-e011-94ca-0030489379b8>
2011-05-11 01:23:33 +0000

    - Add RebuildFileLists. - Fix off-by-one in Unpack.

---
+++ trunk/RebuildFileLists/Breakdown.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RebuildFileLists
+{
+    internal class Breakdown
+    {
+        public long Known = 0;
+        public long Total = 0;
+
+        public int Percent
+        {
+            get
+            {
+                if (this.Total == 0)
+                {
+                    return 0;
+                }
+
+                return (int)Math.Floor((
+                    (float)this.Known /
+                    (float)this.Total) * 100.0);
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}/{1} ({2}%)",
+                this.Known,
+                this.Total,
+                this.Percent);
+        }
+    }
+}
